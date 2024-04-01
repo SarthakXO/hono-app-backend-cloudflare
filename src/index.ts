ddf2b01c0b4b29766715c5583647334101f05f63
Sarthak <sarthakbhatt@live.com>
2024-04-02 04:44:41 +0530

    making post requests

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,12 @@ import { Hono } from "hono";
 
 const app = new Hono();
 
-app.get("/", (c) => {
-  const body = c.req.json();
+app.post("/", async (c) => {
+  const body = await c.req.json();
   console.log(body);
   console.log(c.req.header);
   console.log(c.req.query);
   return c.json({ message: "Hello Hono!" });
-});
-
-app.post("/", (c) => {
-  return c.text("Hello Hono!");
 });
 
 export default app;
