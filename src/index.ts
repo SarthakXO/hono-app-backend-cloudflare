82e3351a3b9dd11deb4485a6ba02afe46e80d00f
Sarthak <sarthakbhatt@live.com>
2024-04-02 04:50:34 +0530

    creating a Auth middleware

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,17 @@ import { Hono } from "hono";
 
 const app = new Hono();
 
+//creating a AUTH middleware
+
+app.use(async (c, next) => {
+  if (c.req.header("Authorization")) {
+    //do validation
+    await next();
+  } else {
+    return c.json({ message: "wrong user" });
+  }
+});
+
 app.post("/", async (c) => {
   const body = await c.req.json();
   console.log(body);
